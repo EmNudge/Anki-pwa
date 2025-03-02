f6fa87ed78c405600a1a30babb93521f883de63e
EmNudge <24513691+EmNudge@users.noreply.github.com>
2025-03-02 12:33:02 -0500

    add media (#2)

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -10,3 +10,7 @@ export function assert(condition: boolean, message: string) {
   if (!condition) throw new Error(message);
 }
 
+export function isTruthy<T>(value: T): value is NonNullable<T> {
+  return !!value;
+}
+
